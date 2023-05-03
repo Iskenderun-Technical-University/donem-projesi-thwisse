933525b5dbdbe8499d8685f523780827a831b1e8
thwisse <thwisse@gmail.com>
2023-05-04 00:26:53 +0300

    readme eklendiii

--- mySQLproject/project1/Form1.Designer.cs
+++ mySQLproject/project1/Form1.Designer.cs
@@ -37,9 +37,10 @@
             this.label1.Font = new System.Drawing.Font("FiraCode Nerd Font Mono", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.Location = new System.Drawing.Point(344, 200);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 28);
+            this.label1.Size = new System.Drawing.Size(124, 28);
             this.label1.TabIndex = 0;
-            this.label1.Text = "thwisse";
+            this.label1.Text = "thwissed";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
